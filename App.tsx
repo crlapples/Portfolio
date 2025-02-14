436e5fc0f29bc0a70990e2f33e9a530e4f0bfb18
crlapples <crlapples19@gmail.com>
2025-02-14 11:45:27 -0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,8 +4,10 @@ import Home from "./Home";
 import Projects from "./Projects";
 import Skills from "./Skills";
 import Resume from "./Resume";
+import Testimonials from "./Testimonials";
 import AboutMe from "./AboutMe";
 import Contact from "./Contact";
+import Order from "./Order";
 
 const App = () => {
 
@@ -16,8 +18,10 @@ const App = () => {
         <Route path="/Projects" element={<Projects />} />
         <Route path="/Skills" element={<Skills />} />
         <Route path="/Resume" element={<Resume />} />
+        <Route path="/Testimonials" element={<Testimonials />} />
         <Route path="/AboutMe" element={<AboutMe />} />
         <Route path="/Contact" element={<Contact />} />
+        <Route path="/Order" element={<Order />} />
       </Routes>
     </Router>
   );
