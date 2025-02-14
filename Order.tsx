f9ddd386ae6733ac38a6d0d73077e94903d3dfbf
crlapples <crlapples19@gmail.com>
2025-02-14 15:12:49 -0800

    Update Order.tsx

--- Order.tsx
+++ Order.tsx
@@ -2,7 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import "./Order.css";
 
-const Order = () => {
+const Order: React.FC = () => {
+  const [selectedPages, setSelectedPages] = useState<string>("");
+
+  const handlePages = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedPages(event.target.value);
+  };
 
   return (
     <main className="mainContainer">
@@ -28,6 +33,23 @@ const Order = () => {
         <header className="titleContainer">
           <h1 className="titleDesc">Your Website, Your Way. Let’s Build It!</h1>
         </header>
+        <div className="formContainer">
+          <form>
+            <div className="groupInput">
+              <label htmlFor="pages">Choose page amount:</label>
+              <select id="pages" name="pages" value={selectedPages} onChange={handlePages}>
+                <option value="1">Single-page, 1</option>
+                <option value="2-5">Small business, 2-5</option>
+                <option value="5-10">Professional, 5-10</option>
+              </select>
+              <label htmlFor="hosting">Need hosting?</label>
+              <select id="hosting" name="hosting">
+                <option value="yes">Yes</option>
+                <option value="no">No</option>
+              </select>
+            </div>
+          </form>
+        </div>
       </div>
     </main>
   );
