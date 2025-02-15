e1095fa0c72223ef7f9f7fa4cb94a8e958ec1cfd
crlapples <crlapples19@gmail.com>
2025-02-14 21:05:57 -0800

    Create Thankyou.tsx

---
+++ Thankyou.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import "./Thankyou.css";
+
+const Thankyou = () => {
+
+  return (
+    <main className="mainContainer">
+      <div className="options">
+        <h2 className="title">Made in React</h2>
+      </div>
+      <div className="contentContainer">
+        <div className="logoContainer">
+          <img className="logo" src="https://assets.onecompiler.app/42xjnjtme/438vttv6t/Gemini_Generated_Image_f55fiof55fiof55f%20(1).jpeg" />
+        </div>
+        <div className="navBarContainer">
+          <ul className="navBar">
+            <li className="home"><Link to="/">Home</Link></li>
+            <li className="projects"><Link to="/Projects">Projects</Link></li>
+            <li className="skills"><Link to="/Skills">Skills</Link></li>
+            <li className="resume"><Link to="/Resume">Resume</Link></li>
+            <li className="testimonials"><Link to="/Testimonials">Reviews</Link></li>
+            <li className="aboutMe"><Link to="/AboutMe">About Me</Link></li>
+            <li className="contact"><Link to="/Contact">Contact Me</Link></li>
+            <li className="order"><Link to="/Order">Order Now</Link></li>
+          </ul>
+        </div>
+        <header className="titleContainer">
+          <h1 className="titleDesc">Transaction For Your Website Completed</h1>
+        </header>
+        <div className="thanksContainer">
+          <p className="thankyou">Thank You</p>
+          <p className="extraInfo">You will be contacted shortly</p>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Thankyou;
