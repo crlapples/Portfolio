54defa87401c86dd30e3a3efabfa92b28b65d0ab
crlapples <crlapples19@gmail.com>
2025-02-15 21:29:00 -0800

    Create Skills.tsx

---
+++ Skills.tsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import "./Skills.css";
+
+const Skills: React.FC = () => {
+
+  return (
+    <main className="mainContainer">
+      <div className="options">
+        <h2 className="title">Made in React</h2>
+      </div>
+      <div className="contentContainer">
+        <div className="logoContainer">
+          <img className="logo" src="https://assets.onecompiler.app/42xjnjtme/438vttv6t/Gemini_Generated_Image_f55fiof55fiof55f%20(1).jpeg" />
+        </div>
+        <div className="navBarContainer">
+          <ul className="navBar">
+            <li className="home"><Link to="/">Home</Link></li>
+            <li className="projects"><Link to="/Projects">Projects</Link></li>
+            <li className="skills"><Link to="/Skills">Skills</Link></li>
+            <li className="resume"><Link to="/Resume">Resume</Link></li>
+            <li className="testimonials"><Link to="/Testimonials">Reviews</Link></li>
+            <li className="aboutMe"><Link to="/AboutMe">About Me</Link></li>
+            <li className="contact"><Link to="/Contact">Contact Me</Link></li>
+            <li className="order"><Link to="/Order">Order Now</Link></li>
+          </ul>
+        </div>
+        <header className="titleContainer">
+          <h1 className="titleDesc">crlapples' Projects</h1>
+        </header>
+        <div classMame="skillsContainer">
+          <div className="skills">
+            <img src="" />
+            <label className="skillName"></label>
+            <img src="" />
+            <label className="skillName"></label>
+            <img src="" />
+            <label className="skillName"></label>
+            <img src="" />
+            <label className="skillName"></label>
+            <img src="" />
+            <label className="skillName"></label>
+            <img src="" />
+            <label className="skillName"></label>
+            <img src="" />
+            <label className="skillName"></label>
+            <img src="" />
+            <label className="skillName"></label>
+            <img src="" />
+            <label className="skillName"></label>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Skills;
