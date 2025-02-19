d49911d01abb15a9803b7f8d3973d723cce09b64
crlapples <crlapples19@gmail.com>
2025-02-19 00:48:52 +0000

    a

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./Home";
 import Projects from "./Projects";
 import Skills from "./Skills";
@@ -13,7 +13,7 @@ import Thankyou from "./Thankyou";
 const App = () => {
 
   return (
-    <Router basename="/Portfolio">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Projects" element={<Projects />} />
